d595e92d4e8b9c13b3ab614e2c0408fc3373dd0e
unknown <cris.sandoval.cf@gmail.com>
2023-04-24 12:55:26 -0300

    b-08 y b-09

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 import express from'express';
-const router = express.Router();
+import userRouter from './users.js'
+import authorRouter from './authors.js'
+import categoryRouter from './categories.js'
+import mangaRouter from './mangas.js'
+import chapterRouter from './chapters.js'
+import companyRouter from './companies.js'
 
-/* GET home page. */
+const router = express.Router();
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', {
+    title: 'MINGA API',
+    subtitle: 'endpoints of minga'
+  })
+})
+
+
+ 
+router.use('/auth', userRouter)
+router.use('/authors',authorRouter)
+router.use('/categories',categoryRouter)
+router.use('/companies',companyRouter)
+router.use('/mangas',mangaRouter)
+router.use('/chapters',chapterRouter)
+
+
 
 export default  router;
